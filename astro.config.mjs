6e1d7c63efcd86e4554356ca569906250e034745
meseguera <alejandro.meseguer@gmail.com>
2022-12-18 09:11:37 -0500

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,3 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({});
+  site: 'https://astro-meseguera.github.io',
+  base: '/astro-meseguera.github.io'
+
+ })
+
